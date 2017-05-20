dd2545fe2170043d85a5612859c3e1ae3d9b40dd
manimaran96 <manimaraninam1027@gmail.com>
2017-05-21 01:49:38 +0530

    global bootstrap link added

--- bootstrap.php
+++ bootstrap.php
@@ -1,12 +1,16 @@
 <!--Declare the default home path-->
 <?php
-$home_dir ="http://localhost/yoga_site";
+$home_dir ="http://yogasana.herokuapp.com/";
 ?>
 
 <!-- Bootstrap -->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="<?php echo $home_dir?>/bootstrap/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-  <script src="<?php echo $home_dir?>/bootstrap/js/bootstrap.min.js"></script>   
-  
+ <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>	  
